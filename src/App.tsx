d71b8d4ca0c55412e9b49ce744df9b9c0205cf74
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-23 19:39:34 +0300

    passing functions vai props

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,13 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   const items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
-  return <ListGroup items={items} heading="Cities" />;
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+  );
 }
 
 export default App;
