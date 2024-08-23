0f504c288972fd17e779316a636ee9d83ea29982
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-23 20:44:29 +0300

    button controling alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
 
 const App = () => {
   const cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+  const [alertVisible, setAlertVisible] = useState(false);
 
   return (
     <div>
-      <Button onClick={() => console.log("Button clicked")}>My button</Button>
+      {alertVisible && (
+        <Alert onCLose={() => setAlertVisible(false)}>My Alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisible(true)}>My button</Button>
       <br />
       <br />
       <ListGroup
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,10 +2,24 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  onCLose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onCLose }: Props) => {
+  return (
+    <>
+      <div className="alert alert-primary alert-dismissible">
+        {children}
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+          onClick={onCLose}
+        />
+      </div>
+    </>
+  );
 };
 
 export default Alert;
