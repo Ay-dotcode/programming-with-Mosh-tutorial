c6f8a8ae4913d756aefba007597964a69e8f3bbc
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-23 19:53:42 +0300

    passing children to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
-function App() {
-  const items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
+const App = () => {
   return (
-    <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+    <div>
+      <Alert>
+        Hello <span>world</span>
+      </Alert>
+    </div>
   );
-}
+};
 
 export default App;
