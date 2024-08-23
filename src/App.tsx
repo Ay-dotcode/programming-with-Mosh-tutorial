9a8b02eb8a502eb6b9f5f2ed4b13867d7f194759
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-23 20:04:50 +0300

    adding listgroup back

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import Alert from "./components/Alert";
+import ListGroup from "./components/ListGroup";
 
 const App = () => {
+  const cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+
   return (
     <div>
       <Alert>
         Hello <span>world</span>
       </Alert>
+
+      <ListGroup
+        heading="Cities"
+        items={cities}
+        onSelectItem={(item) => console.log(item)}
+      />
     </div>
   );
 };
